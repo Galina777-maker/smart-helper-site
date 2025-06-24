eff5eaa38bfb1ffc21e5bb527ee4bf48278bda35
Galina777-maker <karklingalina777@gmail.com>
2025-06-24 17:55:28 +0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Сайт по созданию Telegram-ботов</h1>
+      <Button text="Запустить бота" />
+    </main>
+  );
+}
